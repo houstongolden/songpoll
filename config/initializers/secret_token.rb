43a840cd3f63161706e3271cdadd67477b2d0e7b
Houston Golden <houstongolden@gmail.com>
2013-12-13 12:33:49 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Songpoll::Application.config.secret_key_base = 'f365519424a453c5d6cf4668460de3876fa7bbf58245e8d5bf0921309b7235a422ac8d624f6d12ecf962482e483f1a839be747f5e40c3dd35e3eaa34337f0cb2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Songpoll::Application.config.session_store :cookie_store, key: '_songpoll_session'
